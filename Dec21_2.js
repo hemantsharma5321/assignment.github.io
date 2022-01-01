feb6f788dd246c2d2807fbaf6b41bc947b1b83e4
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-01-01 16:03:07 +0530

    Create Dec21_2.js

---
+++ Dec21_2.js
@@ -0,0 +1,9 @@
+var ch=prompt("enter character");
+
+if(ch=="a" || ch=="e" || ch=="i" || ch=="o" || ch=="u" || ch=="A" || ch=="E" || ch=="I" || ch=="O" || ch=="U")
+{
+    console.log("it is Vowel");
+}
+else{
+    console.log("Not vowel");
+}
