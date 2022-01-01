2665a2633c9d3a0bbf5c26b887647d66b5602a3a
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-01-01 16:31:13 +0530

    Create Dec20_1.js

---
+++ Dec20_1.js
@@ -0,0 +1,11 @@
+var num=prompt("enter a number");
+if(num==10){
+    console.log("exactly 10");
+}
+else if(num>10){
+    console.log("greater than 10");
+    
+}
+else{
+    console.log("less than 10");
+}
