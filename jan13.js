93c6f5a4a64d899b82b077787ab26a7667725846
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-01-20 00:43:27 -0800

    Add files via upload

---
+++ jan13.js
@@ -0,0 +1,10 @@
+const matrixOne =
+[ [4, 3, 2, 4],
+[9, 1, 5, 6], 
+[2, 7, 9, 8],
+[8, 5, 2, 0] ];
+var sum=0;
+for (var i = 0; i < matrixOne.length; i++) {
+    sum=sum+matrixOne[i][i];
+}
+console.log(sum);
